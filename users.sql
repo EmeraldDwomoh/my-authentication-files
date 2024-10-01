0cb51e3b420e271c5d3d56864a94e3172427e499
EmeraldDwomoh <dwomohemerald2013@gmail.com>
2024-09-30 21:26:54 -0700

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 30, 2024 at 07:35 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_info`
+--
+
+CREATE TABLE `personal_info` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `homeAddress` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `signInProvider` varchar(255) NOT NULL,
+  `profliepictureURL` varchar(255) DEFAULT NULL,
+  `verify_token` varchar(255) DEFAULT NULL,
+  `verify_status` tinyint(2) NOT NULL DEFAULT 0 COMMENT '0 = no, 1 = yes',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_login` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_progress`
+--
+
+CREATE TABLE `personal_progress` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL,
+  `topic` varchar(255) NOT NULL,
+  `stage` int(3) NOT NULL,
+  `points` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Table structure for table `student_info`
+--
+
+CREATE TABLE `student_info` (
+  `id` int(11) NOT NULL,
+  `teacher_firstName` varchar(255) NOT NULL,
+  `teacher_lastName` varchar(255) NOT NULL,
+  `teacher_email` varchar(255) DEFAULT NULL,
+  `schoolName` varchar(255) NOT NULL,
+  `schoolAddress` varchar(255) NOT NULL,
+  `student_username` varchar(255) DEFAULT NULL,
+  `student_email` varchar(255) DEFAULT NULL,
+  `student_password` varchar(255) DEFAULT NULL,
+  `signInProvider` varchar(255) NOT NULL,
+  `verify_token` varchar(255) DEFAULT NULL,
+  `verify_status` tinyint(2) NOT NULL DEFAULT 0 COMMENT '0 = no, 1 = yes',
+  `profliepictureURL` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_login` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `student_progress` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL,
+  `topic` varchar(255) NOT NULL,
+  `stage` int(3) NOT NULL,
+  `points` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `personal_info`
+--
+ALTER TABLE `personal_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personal_progress`
+--
+ALTER TABLE `personal_progress`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_info`
+--
+ALTER TABLE `student_info`
+ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_progress`
+--
+ALTER TABLE `student_progress`
+ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `personal_info`
+--
+ALTER TABLE `personal_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personal_progress`
+--
+ALTER TABLE `personal_progress`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+--
+-- AUTO_INCREMENT for table `student_info`
+--
+ALTER TABLE `student_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+--
+-- AUTO_INCREMENT for table `student_progress`
+--
+ALTER TABLE `student_progress`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
